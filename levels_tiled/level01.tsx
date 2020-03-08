51ad9943e0b9e2613c197a0f595d2b26062fd862
inchko <inchko@gmail.com>
2020-03-08 17:44:29 +0100

    1ra version mapa lvl1

---
+++ levels_tiled/level01.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="level01" tilewidth="32" tileheight="32" tilecount="120" columns="15">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../baba_is_you/images/test.png" width="509" height="282"/>
+</tileset>
---
+++ levels_tiled/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="test" tilewidth="16" tileheight="16" tilecount="527" columns="31">
+ <image source="../baba_is_you/images/test.png" width="509" height="282"/>
+</tileset>
